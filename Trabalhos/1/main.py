223883332511b0acedc316872fa3e05f4e2a9be8
João Winckler <jpwbernardi@hotmail.com>
2017-03-21 21:42:35 -0300

    Pronto

--- Trabalhos/1/main.py
+++ Trabalhos/1/main.py
@@ -1,15 +1,32 @@
-from bottle import route, run, template, post, request
+from bottle import run, get, post, view, request, redirect, route, static_file
+import bottle
 
 messages = []
 
-@route('/chat', method="GET")
-@route('/chat', method="POST")
-def chat():
-    messages.append(request.query.msg)
-    return messages
+@bottle.route('/static/<path:path>')
+def server_static(path):
+    return static_file(path, root='static')
 
-run(host='localhost', port=8080)
+@get('/chat')
+@view('chat')
+def chat():
+    return dict(msg=messages)
 
 @route('/')
 def index():
-    return 
+    return
+
+@get('/client')
+@post('/client')
+@view('client')
+def renderClient():
+    if request.forms.get('name') == '' or request.forms.get('msg') == '':
+        print('Olá')
+        return dict(error=True)
+    elif request.forms.get('name') != None and request.forms.get('msg') != None:
+        print('Tchaaaau')
+        messages.append(request.forms.get('name') + ': ' + request.forms.get('msg'))
+    return dict(error=False)
+run(host='localhost', port=8080)
+
+
